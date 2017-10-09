1abfe29c50b887c85c026604a75b4328e30ecf51
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-10-09 21:52:12 +0200

    implement help and gouser inputs

--- Game.cpp
+++ Game.cpp
@@ -7,7 +7,7 @@ Player player;
 void Game::init()
 {
     loadAreas();
-    player.setPosition(4);
+    player.setPosition(0);
 }
 
 void Game::loadAreas()
@@ -34,6 +34,124 @@ void Game::loadAreas()
     areas.emplace_back("End", "End room", dirs6);
 }
 
+void Game::printHelp() {
+    std::cout<<"Help: you can use case insensitive 'h' or 'help' for this message, or"<<std::endl;
+    std::cout<<"'g' or 'go' n/north/e/east/s/south/w/west to go to the given direction"<<std::endl;
+    std::cout<<"or..."<<std::endl;
+}
+
+//TODO remove code duplicates
+void Game::moveTo(const std::string& dir) {
+    std::cout<<"GO"<<std::endl;
+    if (dir == "n" || dir == "north") {
+        if (areas[player.getPosition()].getValidDirs()[0] != -1) {
+            player.setPosition(areas[player.getPosition()].getValidDirs()[0]);
+            run();
+        } else {
+            std::cout << "Not valid direction" << std::endl;
+            run();
+        }
+    }
+    if (dir == "e" || dir == "east") {
+        if (areas[player.getPosition()].getValidDirs()[1] != -1) {
+            player.setPosition(areas[player.getPosition()].getValidDirs()[1]);
+            run();
+        } else {
+            std::cout << "Not valid direction" << std::endl;
+            run();
+        }
+    }
+    if (dir == "s" || dir == "south") {
+        if (areas[player.getPosition()].getValidDirs()[2] != -1) {
+            player.setPosition(areas[player.getPosition()].getValidDirs()[2]);
+            run();
+        } else {
+            std::cout << "Not valid direction" << std::endl;
+            run();
+        }
+    }
+    if (dir == "w" || dir == "west") {
+        if (areas[player.getPosition()].getValidDirs()[3] != -1) {
+            player.setPosition(areas[player.getPosition()].getValidDirs()[3]);
+            run();
+        } else {
+            std::cout << "Not valid direction" << std::endl;
+            run();
+        }
+    }
+}
+
+void Game::handleUserInput() {
+    string input;
+    bool validInput;
+    bool isValidFirstWord = false;
+    std::string firstWord;
+    std::string restWords;
+    do {
+        firstWord = "";
+        restWords = "";
+        std::string lowerInput = "";
+        validInput = true;
+        std::cout<<"What are you going to do next? ";
+        getline(std::cin, input);
+        for (char c: input) {
+            if ( (c < 'A' && c != ' ') || c > 'z' || (c > 'Z' && c < 'a') ) {
+                std::cout<<"Sorry, there mist be a typo somewhere :-("<<std::endl;
+                validInput = false;
+                break;
+            } else if (c == ' ' && lowerInput == "") {
+                continue;
+            } else if ( c >= 'A' && c <= 'Z') {
+                lowerInput += c + 'z' - 'Z';
+                std::cout<<"lower char "<<lowerInput<<std::endl;
+            } else {
+                lowerInput += c;
+            }
+        }
+        if ( !validInput) {
+            continue;
+        }
+        std::cout<<"Your input is "<<lowerInput<<std::endl;
+        int spacePos = lowerInput.find(' ');
+        if (spacePos == -1) {
+            firstWord = lowerInput;
+        } else {
+            firstWord = lowerInput.substr(0, spacePos);
+            restWords = lowerInput.substr(spacePos+1, lowerInput.size());
+            //TODO rmeove spaces from the begon amd end of restWords
+        }
+        std::cout<<"firstWord "<<firstWord<<std::endl;
+        if (restWords != "") {
+            std::cout<<"restWords "<<restWords<<std::endl;
+        }
+        std::vector<std::string> validFirstWords = {"h", "help",
+                                                    "g", "go",
+                                                    "u", "use",
+                                                    "e", "eat",
+                                                    "f", "fight"};
+
+        for (std::string word : validFirstWords) {
+            if (firstWord == word) {
+                std::cout << "Valid first word" << std::endl;
+                isValidFirstWord = true;
+                break;
+            }
+        }
+        if ( !isValidFirstWord) {
+            std::cout<<"Sorry, I can not understand :-("<<std::endl;
+        }
+    } while ( !isValidFirstWord );
+
+    if (firstWord == "h" || firstWord == "help") {
+        printHelp();
+        Game::handleUserInput();
+    } else if (firstWord == "g" || firstWord == "go") {
+        Game::moveTo(restWords);
+        Game::handleUserInput();
+    }
+
+}
+
 void Game::run()
 {
     while(!step()) {
@@ -52,11 +170,18 @@ void Game::run()
             }
         }
         std::cout<<dir<<std::endl;
+        printHelp();
+        //TODO refactor infinit while true loop
+        while(true) {
+            handleUserInput();
+        }
         break;
     }
 }
 
+//TODO refactor step
 bool Game::step()
 {
     return false;
 }
+
